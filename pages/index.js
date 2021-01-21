7acd71820221a25475db641f153d5228a577b224
Arnold Restrepo <arnold.restrepohernandez@gmail.com>
2021-01-20 19:45:49 -0500

    App Layout - Styles

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import AppLayout from '../components/AppLayout';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.Container}>
       <Head>
         <title>Devter</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1 className={styles.title}>
-        <a href='https://nextjs.org'>Devter</a>
-      </h1>
-      <Link href='/timeline'>Timeline</Link>
+      <AppLayout>
+        <h1 className={styles.title}>
+          <a href='https://nextjs.org'>Devter</a>
+        </h1>
+        <Link href='/timeline'>Timeline</Link>
+      </AppLayout>
     </div>
   );
 }
