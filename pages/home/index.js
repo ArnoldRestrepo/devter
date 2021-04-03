b99790829938cbcba7ae3b415086caea9938a901
Arnold Restrepo <arnold.restrepohernandez@gmail.com>
2021-04-03 00:17:18 -0500

    Feature/timeline-fetch

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
+import { v4 as uuidv4 } from 'uuid'
 import AppLayout from '../../components/AppLayout'
 import Avatar from '../../components/Avatar'
 import styles from '../../styles/HomePage.module.css'
 
 export default function HomePage() {
+  const [timeline, setTimeline] = useState([])
+  const [load, setLoad] = useState(false)
+
+  useEffect(() => {
+    setLoad(true)
+    const API_URL = `http://localhost:3000`
+    const getTimeline = async () => {
+      const response = await fetch(`${API_URL}/api/statuses/home_timeline`)
+      const data = await response.json()
+      setTimeline(data)
+      setLoad(false)
+    }
+    getTimeline()
+  }, [])
+
   return (
     <section className="Container">
       <AppLayout>
@@ -19,8 +36,17 @@ export default function HomePage() {
           />
           <h2 className={styles.Title}>Inicio</h2>
         </header>
-        <section>
-          <p>Hello</p>
+        <section className={styles.TweetGrid}>
+          {load && <p>Cargando...</p>}
+          {timeline?.map(tweet => {
+            return (
+              <article key={uuidv4()} className={styles.tweet}>
+                <Avatar alt={tweet.username} src={tweet.avatar} isRounded />
+                <p>{tweet.message}</p>
+                <span>@{tweet.username}</span>
+              </article>
+            )
+          })}
         </section>
         <nav className={styles.Menu}>
           <p>Home</p>
