aae1b655da28496fb232b9edae02e38ed1e12f6d
Arnold Restrepo <arnold.restrepohernandez@gmail.com>
2021-04-07 23:57:44 -0500

    feature/create-devit-in-firestore

--- pages/compose/tweet/index.js
+++ pages/compose/tweet/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 import AppLayout from 'components/AppLayout'
 import styles from '../../../styles/Form.module.css'
 import Button from '../../../components/Button'
@@ -6,9 +7,18 @@ import useUser from 'hooks/useUser'
 
 import { addDevit } from '../../../firebase/client'
 
+const COMPOSE_STATES = {
+  USER_NOT_KNOWN: Symbol('USER_NOT_KNOWN'),
+  SUCCESS: Symbol('SUCCESS'),
+  LOADING: Symbol('LOADING'),
+  ERROR: Symbol('ERROR')
+}
+
 export default function ComposeTweet() {
+  const router = useRouter()
   const user = useUser()
   const [message, setMessage] = useState('')
+  const [status, setStatus] = useState(COMPOSE_STATES.USER_NOT_KNOWN)
 
   const handleMessage = ({ target }) => {
     const { value } = target
@@ -17,13 +27,23 @@ export default function ComposeTweet() {
 
   const handleSubmit = event => {
     event.preventDefault()
+    setStatus(COMPOSE_STATES.LOADING)
     addDevit({
       avatar: user.avatar,
       content: message,
       userId: user.uid,
       userName: user.username
     })
+      .then(() => {
+        router.push('/home')
+      })
+      .catch(error => {
+        setStatus(COMPOSE_STATES.ERROR)
+        console.error('Error at create Devit in DB', error)
+      })
   }
+
+  const isButtonDisabled = !message.length || status === COMPOSE_STATES.LOADING
 
   return (
     <main className="Container">
@@ -34,7 +54,7 @@ export default function ComposeTweet() {
             value={message}
             onChange={handleMessage}
           ></textarea>
-          <Button disabled={message.length === 0}>Devitear</Button>
+          <Button disabled={isButtonDisabled}>Devitear</Button>
         </form>
       </AppLayout>
     </main>
