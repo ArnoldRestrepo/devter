7d4134c7a1d5efb7e423915342778e2ec8d99017
Arnold Restrepo Hernandez <arnold.restrepohernandez@gmail.com>
2021-04-24 18:17:39 -0500

    feature/grid-layout-home

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 import Head from 'next/head'
 import { v4 as uuidv4 } from 'uuid'
 import AppLayout from '../../components/AppLayout'
@@ -8,6 +9,7 @@ import Tweet from 'components/Tweet'
 import useUser from 'hooks/useUser'
 import { fetchLatestDevits } from 'firebase/client'
 import { isEmptyObject } from 'utils'
+import Create from 'components/Icons/Create'
 
 export default function HomePage() {
   const user = useUser()
@@ -25,37 +27,50 @@ export default function HomePage() {
   return (
     <section className="Container">
       <AppLayout>
-        <Head>
-          <title>Home Page</title>
-        </Head>
-        <header className={styles.Header}>
-          <Avatar
-            alt="Avatar"
-            src="https://images-na.ssl-images-amazon.com/images/M/MV5BNjk5NjE5NTczMV5BMl5BanBnXkFtZTcwODI0OTM0NA@@._V1_UY256_CR4,0,172,256_AL_.jpg"
-            height="50"
-            isRounded
-          />
-          <h2 className={styles.Title}>Inicio</h2>
-        </header>
-        <section className={styles.TweetGrid}>
-          {load && <p>Cargando...</p>}
-          {isEmptyObject(timeline) && <p>No hemos podido ver los Devits...</p>}
-          {timeline?.map(({ userId, userName, content, avatar, createdAt }) => {
-            return (
-              <Tweet
-                key={uuidv4()}
-                userId={userId}
-                userName={userName}
-                content={content}
-                avatar={avatar}
-                createdAt={createdAt}
+        <div className={styles.HomeLayout}>
+          <Head>
+            <title>Home Page</title>
+          </Head>
+          <header className={styles.Header}>
+            <Avatar
+              alt="Avatar"
+              src="https://images-na.ssl-images-amazon.com/images/M/MV5BNjk5NjE5NTczMV5BMl5BanBnXkFtZTcwODI0OTM0NA@@._V1_UY256_CR4,0,172,256_AL_.jpg"
+              height="50"
+              isRounded
+            />
+            <h2 className={styles.Title}>Inicio</h2>
+          </header>
+          <section className={styles.TweetGrid}>
+            {load && <p>Cargando...</p>}
+            {isEmptyObject(timeline) && (
+              <p>No hemos podido ver los Devits...</p>
+            )}
+            {timeline?.map(
+              ({ userId, userName, content, avatar, createdAt }) => {
+                return (
+                  <Tweet
+                    key={uuidv4()}
+                    userId={userId}
+                    userName={userName}
+                    content={content}
+                    avatar={avatar}
+                    createdAt={createdAt}
+                  />
+                )
+              }
+            )}
+          </section>
+          <nav className={styles.Menu}>
+            <Link href="/compose/tweet" className={styles.MenuLink}>
+              <Create
+                stroke="#09f"
+                width="32"
+                height="32"
+                className={styles.MenuIcon}
               />
-            )
-          })}
-        </section>
-        <nav className={styles.Menu}>
-          <p>Hello</p>
-        </nav>
+            </Link>
+          </nav>
+        </div>
       </AppLayout>
     </section>
   )
