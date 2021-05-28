68ea6bd1230fea8f4d5e12533f01b4f4a8d1b44a
Arnold Restrepo Hernandez <arnold.restrepohernandez@gmail.com>
2021-05-27 20:05:55 -0500

    Header Avatar

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,14 +1,27 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Avatar from '../Avatar'
 import styles from '../../styles/Header.module.css'
+import useUser from 'hooks/useUser'
 export default function Header() {
+  const user = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    user && router.replace('/home')
+  }, [user])
+
   return (
     <header className={styles.Header}>
-      <Avatar
-        alt="Avatar"
-        src="https://images-na.ssl-images-amazon.com/images/M/MV5BNjk5NjE5NTczMV5BMl5BanBnXkFtZTcwODI0OTM0NA@@._V1_UY256_CR4,0,172,256_AL_.jpg"
-        height="50"
-        isRounded
-      />
+      {user && user.avatar && (
+        <Avatar
+          src={user.avatar}
+          alt={user.username}
+          text={user.username}
+          height="50"
+          isRounded
+        />
+      )}
       <h2 className={styles.Title}>Inicio</h2>
     </header>
   )
